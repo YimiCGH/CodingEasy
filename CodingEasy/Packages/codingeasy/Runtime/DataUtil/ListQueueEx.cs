998dfd91108d3dcddaa707dc9f89710b2abdf210
yimi <455022370@qq.com>
2020-01-03 00:06:55 +0800

    添加List的队列扩展

---
+++ CodingEasy/Packages/codingeasy/Runtime/DataUtil/ListQueueEx.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace YimiTools.DataUtil
+{
+    public static class ListQueueEx
+    {
+        public static T Dequeue<T>(this List<T> list)
+        {
+            T local = list[0];
+            list.RemoveAt(0);
+            return local;
+        }
+        public static void Enqueue<T>(this List<T> list, T _Item)
+        {
+            list.Add(_Item);
+        }
+        public static T Pick<T>(this List<T> list) {
+
+            return list[0];
+        }
+    }
+}
---
+++ CodingEasy/Packages/codingeasy/Runtime/DataUtil/ListStackEx.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace YimiTools.DataUtil
+{
+    public static class ListStackEx
+    {
+        public static T Pop<T>(this List<T> list)
+        {
+            int index = list.Count - 1;
+            T local = list[index];
+            list.RemoveAt(index);
+            return local;
+        }
+        public static void Push<T>(this List<T> list, T _Item)
+        {
+            list.Add(_Item);
+        }
+    }
+}
