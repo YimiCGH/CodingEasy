fc76606789ad74f557ff92e42f934d2c20e4b772
Yimi <455022370@qq.com>
2020-05-05 14:25:52 +0800

    添加相机控制和网格合并脚本
    1.仿场景相机控制脚本
    2.自动合并网格

---
+++ CodingEasy/Packages/codingeasy/Runtime/MeshUtils/MergeMesh.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using CastShadowMode = UnityEngine.Rendering.ShadowCastingMode;
+
+public class MergeMesh : MonoBehaviour
+{
+    public Material Mat;
+    public StaticEditorFlags Flags;
+    public bool DestroyChildren;
+    public bool CreateMeshCollider;
+
+    public CastShadowMode CastShadow;
+
+    public bool ReciveShadows;
+    public ReceiveGI GI;
+    public bool ReciveGI;
+
+    private Mesh _combineMesh;
+    void Awake()
+    {
+        Debug.Log("Combine meshes");
+        DoMerge();
+    }
+
+    private void OnDestroy()
+    {
+        if (_combineMesh != null) {
+            DestroyImmediate(_combineMesh);
+            _combineMesh = null;
+        }
+    }
+
+    private void DoMerge() {
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combineInstances = new CombineInstance[meshFilters.Length];
+
+        Matrix4x4 matrix = transform.worldToLocalMatrix;
+
+        for (int i = 0; i < meshFilters.Length; i++)
+        {
+            combineInstances[i].mesh = meshFilters[i].sharedMesh;
+            combineInstances[i].transform = matrix * meshFilters[i].transform.localToWorldMatrix;
+        }
+
+        _combineMesh = new Mesh();
+        _combineMesh.CombineMeshes(combineInstances);
+
+        var meshfilter = gameObject.AddComponent<MeshFilter>();
+        meshfilter.sharedMesh = _combineMesh;
+
+        var renderer = gameObject.AddComponent<MeshRenderer>();
+        renderer.material = Mat;
+        renderer.shadowCastingMode = CastShadow;
+        renderer.receiveShadows = ReciveShadows;
+
+        bool contributeGI = ((int)Flags & (int)StaticEditorFlags.ContributeGI) != 0;
+        if (contributeGI)
+        {
+            renderer.receiveGI = GI;
+        }
+        
+
+
+        if (CreateMeshCollider) {
+            var meshCollider = gameObject.AddComponent<MeshCollider>();
+            meshCollider.sharedMesh = _combineMesh;            
+        }
+
+        if (DestroyChildren)
+        {
+            for (int i = 0; i < meshFilters.Length; i++)
+            {
+                DestroyImmediate(meshFilters[i].gameObject);
+            }
+        }
+        else {
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+
+        
+    }
+}
