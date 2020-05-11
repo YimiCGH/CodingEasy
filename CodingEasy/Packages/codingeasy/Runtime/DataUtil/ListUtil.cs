dbe1a62eb9d677b756c96598b075203317744abf
Yimi <455022370@qq.com>
2020-05-11 22:56:58 +0800

    List扩展
    随机打乱列表顺序

---
+++ CodingEasy/Packages/codingeasy/Runtime/DataUtil/ListUtil.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class ListUtil
+{
+    public static void RandomSort<T>(this List<T> list) {
+        var random = new System.Random();
+        var sortList = ListPool<T>.Get();
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                sortList.Insert(random.Next(sortList.Count), list[i]);
+            }
+            list.Clear();
+            list.AddRange(sortList);
+        }
+        ListPool<T>.Add(sortList);
+    }
+}
